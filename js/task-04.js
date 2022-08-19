45b5d9c3ee35ba09b5b4a8c1e3a6532ef6b64c5f
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-19 20:25:19 +0300

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -14,3 +14,39 @@
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
 
+const counterValue = {
+    value: 0,
+    increment() {
+        this.value +=1;
+    },
+    decrement() {
+        this.value -=1; 
+    }
+};
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+console.log(decrementBtn);
+console.log(incrementBtn);
+console.log(valueEl);
+
+decrementBtn.addEventListener('click', function() {
+    // console.log('Click decrement');
+
+    counterValue.decrement();
+    // console.log(counterValue);
+
+    valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener('click', function() {
+    // console.log('Click increment');
+
+    counterValue.increment();
+    // console.log(counterValue);
+
+    valueEl.textContent = counterValue.value;
+});
+
