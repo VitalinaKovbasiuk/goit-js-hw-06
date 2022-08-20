1dd46d241ca37624e39945c2669d99ee2ddb5084
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-20 21:51:40 +0300

    task 7

--- js/task-07.js
+++ js/task-07.js
@@ -5,3 +5,32 @@
 // <input id="font-size-control" type="range" min="16" max="96" />
 // <br />
 // <span id="text">Abracadabra!</span>
+
+
+const input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+input.addEventListener("input", handleInputRange);
+function handleInputRange(event) {
+  span.style.fontSize = event.currentTarget.value + "px";
+}
+
+
+// const inputEl = document.querySelector('#font-size-control');
+// const textEl = document.querySelector('#text');
+
+// inputEl.addEventListener('input', onFontSizeControl);
+
+// function onFontSizeControl(event) {
+//     const fontSize = this.value;
+//     textEl.style.fontSize = `${fontSize}px`;
+// }
+
+
+// const inputRef = document.querySelector('#font-size-control');
+// const spanRef = document.querySelector('#text');
+
+// inputRef.addEventListener('input', OnInputChange);
+
+// function OnInputChange(event) {   
+//     spanRef.style.fontSize = `${inputRef.value}px`;
+// }
