c61b305d944c19731acfbc8c1bef9353889ac2a2
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-20 18:46:03 +0300

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,18 @@
 // Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
 
 
+const refs = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-output'),
+}
+
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event){
+console.log(event.currentTarget.value);
+refs.span.textContent = event.currentTarget.value;
+
+if (event.currentTarget.value === "") {
+    return refs.span.textContent = "Anonymous";
+}
+}
