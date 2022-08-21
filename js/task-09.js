445b3f3191a5e1e8ea6ccb9594c23771c629d35e
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-21 20:28:59 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -8,14 +8,22 @@
 
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
 
 
+const bodyEl = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+
+button.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  span.textContent = `${getRandomHexColor()}`
+}
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
